0969b76fd023e3731ae86b44fa47b86dc7a32378
Brian Ng <ngshunchiang@hotmail.com>
2022-07-27 17:23:31 +0800

    add create blog page

--- src/Create.js
+++ src/Create.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Create() {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('');
+
+
     return (
         <div className='create'>
-            Create
+            <h2>Create Blog</h2>
+            <form>
+                <label>Blog title:</label>
+                <input type='text' required value={title} onChange={(e) => setTitle(e.target.value)} />
+                <label>Blog Body:</label>
+                <textarea required value={body} onChange={(e) => setBody(e.target.value)} />
+                <label>Author:</label>
+                <select required value={author} onChange={(e) => setAuthor(e.target.value)} >
+                    <option value='mario'>mario</option>
+                    <option value='yoshi'>yoshi</option>
+                </select>
+                <button>Add Blog</button>
+            </form>
+
         </div>
     )
 }
